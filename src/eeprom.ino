ad776603fe5b01035b5ac02d35fc0d695cbcec7c
RedFantom <redfantom@outlook.com>
2017-06-01 17:48:29 +0200

    Move EEPROM funtions to new file

---
+++ src/eeprom.ino
@@ -0,0 +1,22 @@
+// Written by RedFantom for Red's Wake-Up Light
+// Copyright (C) RedFantom 2017
+// Available under the GNU GPLv3 license or any later version
+// See LICENSE in the root of the repository for details
+
+// EEPROM Data addresses
+// TODO Implement Wear levelling to improve EEPROM life expectancy
+const uint8_t alarmOneMinuteAddress = 0x00;
+const uint8_t alarmOneHourAddress = 0x01;
+const uint8_t alarmTwoMinuteAddress = 0x02;
+const uint8_t alarmTwoHourAddress = 0x03;
+const uint8_t alarmOneRadioChannel = 0x04; // This takes up two uint8_ts
+const uint8_t alarmTwoRadioChannel = 0x06; // This takes up two uint8_ts
+const uint8_t alarmOneLampBrightness = 0x08;
+const uint8_t alarmTwoLampBrightness = 0x09;
+const uint8_t alarmOneRadioVolume = 0x10;
+const uint8_t alarmTwoRadioVolume = 0x011;
+
+// Functions for the EEPROM chip
+void readEEPROMData(){
+
+}
