63e66dd11f7c87a32aed132ae7480d24cffcff91
RedFantom <redfantom@outlook.com>
2017-06-01 17:48:41 +0200

    Added buttons.ino

---
+++ src/buttons.ino
@@ -0,0 +1,4 @@
+// Written by RedFantom for Red's Wake-Up Light
+// Copyright (C) RedFantom 2017
+// Available under the GNU GPLv3 license or any later version
+// See LICENSE in the root of the repository for details
